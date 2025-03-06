d7197e28f06eb44752ec82a247af024f8f7496cc
Damir <90578322+Xeney@users.noreply.github.com>
2025-03-06 14:40:55 +0400

    Create main.go

---
+++ main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"math/rand"
+	"sync"
+	"time"
+)
+
+func longRunningTask(ctx context.Context, id int, wg *sync.WaitGroup) {
+	defer wg.Done()
+
+	fmt.Printf("Task %d started\n", id)
+
+	select {
+	case <-time.After(time.Duration(1+rand.Intn(5)) * time.Second):
+		fmt.Printf("Task %d finished\n", id)
+	case <-ctx.Done():
+		fmt.Printf("Task %d canceled\n", id)
+	}
+}
+
+func RunTask() {
+
+}
+
+func main() {
+	rand.Seed(time.Now().UnixNano())
+	var wg sync.WaitGroup
+
+	// Создаем контекст с таймаутом 3 секунды
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	// Запускаем 5 горутин
+	for i := 1; i <= 5; i++ {
+		wg.Add(1)
+		go longRunningTask(ctx, i, &wg)
+	}
+
+	// Ожидаем завершения всех горутин
+	wg.Wait()
+	fmt.Println("Program finished")
+}
